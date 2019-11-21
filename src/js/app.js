b861d42a3e5bbd599f2034f04514f189e1f19a9d
Aadil Khan <aadil.khan@nagarro.com>
2019-11-21 15:40:48 +0530

    app js

--- src/js/app.js
+++ src/js/app.js
@@ -88,7 +88,7 @@ $(document).ready(function () {
     });
 });
 // ======================= JSON RESPONCE TO LOAD DATA BY FETCH
-const jsonURL = "../assets/js/db.json";
+const jsonURL = "https://aadil59.github.io/db.json";
 const getData = paramFilter => {
     $("tbody").html("");
     fetch(jsonURL)
